2d2060e1b25e149b267c3801df9a7fa30b0af6f9
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-06-19 10:22:29 +0700

    Update CPPKT2104 - DIA CHI IP.cpp

--- CPPKT2104 - DIA CHI IP.cpp
+++ CPPKT2104 - DIA CHI IP.cpp
@@ -1,43 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool kt(string s){ //Hàm check xâu số nhỏ hơn bằng 3 chữ số
-	if(s.size()>=4) return false;//Quá 3 chữ số đương nhiên sai
-	for(char i: s){
-		if(!isdigit(i)) return false; //Không là xâu số cũng sai
-	}
-	return true;
+
+bool isInteger(string s) {
+    try {
+        stoi(s);
+        return true;
+    } catch (exception e) {
+        return false;
+    }
 }
 
-int main(){
-	int t;
-	cin>>t;
-	while(t--){
-		string s;
-		cin>>s;
-		for(int i = 0;i<s.size();i++){
-			if(s[i]=='.') s[i]=' ';
-		}
-		stringstream ss(s);
-		string token;
-		vector<string>vs;
-		while(ss>>token) vs.push_back(token);
-		if(vs.size()!=4){
-			cout<<"NO"<<endl;
-		} else {
-			int check = 1;
-			for(int i = 0;i<vs.size();i++){
-				if(!kt(vs[i])){
-					check = 0;
-					break;
-				}
-				int x = stoi(vs[i]);
-				if(x<0||x>255){
-					check = 0;
-					break;
-				}
-			}
-			if (!check) cout<<"NO"<<endl;
-			else cout<<"YES"<<endl;
-		}
-	}
+bool isValidIP(const string& s) {
+    string temp = s;
+    for (char& c : temp) {
+        if (c == '.') c = ' ';
+    }
+    stringstream ss(temp);
+    string token;
+    vector<string> vs;
+    while (ss >> token) vs.push_back(token);
+    if (vs.size() != 4) return false;
+    for (string part : vs) {
+        if (!isInteger(part)) return false;
+        int num = stoi(part);
+        if (num < 0 || num > 255) return false;
+    }
+    return true;
 }
+
+int main() {
+    string s;
+    cin>>s;
+    if(isValidIP(s)) cout<<"YES";
+    else cout<<"NO";
+}
+
