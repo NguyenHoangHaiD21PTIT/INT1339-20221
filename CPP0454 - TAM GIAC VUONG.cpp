7fd1850a57fbf532b508a004f04f73d8f2a57837
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-07-04 22:27:52 +0700

    Update CPP0454 - TAM GIAC VUONG.cpp

--- CPP0454 - TAM GIAC VUONG.cpp
+++ CPP0454 - TAM GIAC VUONG.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
 	int t;
 	cin>>t;
@@ -8,23 +9,27 @@ int main(){
 		cin>>n;
 		long long a[n];
 		vector<long long>v;
-		for(int i=0;i<n;i++){
+		for(long long i=0;i<n;i++){
 			cin>>a[i];
 			v.push_back(a[i]*a[i]);
 		}
 		sort(v.begin(),v.end());
-		int check=0;
-		for(int i=n-2;i>=2;i--){
-			int l=0, r=i-1;
+		int check = 0;
+		for(long long i=n-1;i>=2;i--){
+			int check1 = 0;//ton tai cap so co tong = v[i]
+			//Kiem tra xem co cap so tong  = v[i]?
+			int l = 0, r = i - 1;
 			while(l<r){
-				if(a[l]+a[r]==a[i]){
-					check=1;
+				if(v[l]+v[r]==v[i]) {
+					check1 = 1;
 					break;
-				} else if(a[l]+a[r]<a[i]){
-					l++;
-				} else {
-					r--;
 				}
+				if(v[l]+v[r]<v[i]) l++;
+				if(v[l]+v[r]>v[i]) r--;
+			}
+			if(check1==1){
+				check=1;
+				break;
 			}
 		}
 		if(check==1){
