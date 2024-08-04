cd0f82623971ec9af4c20768cd9cea31f508a6fd
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-08-04 08:46:56 +0700

    Update CPPKT2111. MUC LUC.cpp

--- CPPKT2111. MUC LUC.cpp
+++ CPPKT2111. MUC LUC.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-int Page(string s){//Hàm lấy số trang
+int Page(string s){//Hàm lấy số trang, ví dụ: Page 6
     if(s.size() < 6) return -1;
     string tmp = s.substr(0, 4), page = s.substr(5);
     if(tmp != "Page") return -1;
@@ -15,11 +15,11 @@ int Level(string s){//Hàm lấy cấp độ đề mục
 }
 
 int main(){
-    ifstream fin("VANBAN.txt");
-    ofstream fout("MUCLUC.txt");
+    ifstream cin("VANBAN.txt");
+    ofstream cout("MUCLUC.txt");
     string s;
     int i;//i lưu số trang của tiêu mục
-    while(getline(fin, s)){
+    while(getline(cin, s)){
         if(s=="") continue;//Dòng trống thì kệ không làm gì cả
         int soTrang = Page(s);//Lấy ra số trang tương ứng nếu có
         if(soTrang != -1) i = soTrang; //Gán số trang, chờ cấp độ và tiêu mục tương ứng để ghép
@@ -27,10 +27,9 @@ int main(){
         if(capDo != -1){
             string tmp = "";
             while(capDo--) tmp += ">";//Bao nhiêu cấp thì bấy nhiêu dấu
-            string sub = s.substr(8);
+            string sub = s.substr(8);//Lấy ra nội dung
             tmp += sub + "--- " + to_string(i);
-            fout<<tmp<<endl;
+            cout<<tmp<<endl;
         }
     }
 }
-
