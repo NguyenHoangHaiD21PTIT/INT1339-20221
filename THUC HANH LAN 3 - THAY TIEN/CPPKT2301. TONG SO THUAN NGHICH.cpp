e8a187570dcefa612ed7c12538c1c122fa3d2b2c
Nguyễn Hoàng Hải <158180998+NguyenHoangHaiD21PTIT@users.noreply.github.com>
2024-12-05 11:51:58 +0700

    /

--- THUC HANH LAN 3 - THAY TIEN/CPPKT2301. TONG SO THUAN NGHICH.cpp
+++ THUC HANH LAN 3 - THAY TIEN/CPPKT2301. TONG SO THUAN NGHICH.cpp
@@ -17,12 +17,14 @@ int main() {
 	cout << tong << endl;
 }
 /*
-Ví dụ số có 6 chữ số. Ban đầu sẽ có 900 số [sum(10^2, 999) * 1000] (k = 3)
+Ví dụ số có 6 chữ số. Ban đầu sẽ có 900 số [sum(10^2, 999) * 1000] (k = 3) (Gen phần đầu là tất cả các số có 3 chữ số)
 100 000
 101 000
 ...
 999 000 
 Như vậy thì 900 phần đầu tương ứng với nó sẽ ghép với 900 số đảo có 3 chữ số cộng vào. Mỗi một phần đầu thì chỉ có 1 số đảo ghép được
-Ví dụ: 123 000 --> 123 000 + 321
+Ví dụ: 123 000 --> 123 000 + 321 = 123 321
+Nhưng, có 900 số. Nhưng bộ số để ghép vào phần sau là từ 001 đến 999, mỗi số là 1 số đảo nào đó của phần đầu
+Tất cả những số có 2 chữ số (ít hơn bộ số ghép 1 chữ số) nhân thêm 10 sẽ tạo ra những số "vô dụng" trong bộ số 
 Tuy nhiên, những phần đuôi chứa số 0 sẽ không thể ghép với bất kỳ số nào, vì đảo lại thì ra 0 ở đầu (Vô lí), đây chính là những số ít hơn rìght 1 chữ số nhân với 10
 */
